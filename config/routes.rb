d3af257b976bda23d8cacd6f485b878837273788
bozzhik <bozzhik@ya.ru>
2023-10-04 04:14:59 +0300

    add `about` page + `footer` + `menubar` partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
