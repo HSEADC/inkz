625a3730290eb104cec718f575b09b87b47667b5
bozzhik <bozzhik@ya.ru>
2023-10-04 06:04:35 +0300

    add `Pico.css` for default styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+    @color = generate_random_hex_color
+  end
+
+  def about
+    @color = generate_random_hex_color
+  end
+
+  private
+
+  def generate_random_hex_color
+    '#' + (1..6).map { '0123456789abcdef'.chars.sample }.join
   end
 end
