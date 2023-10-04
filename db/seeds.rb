5ccbab8ed1c06af420707ed592d731de2e507d7c
bozzhik <bozzhik@ya.ru>
2023-10-04 08:49:50 +0300

    add content fill using `seeds`
    fill with content using `rake db:seed`

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,84 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+@masters_data = [
+  { name: 'Макс', nickname: 'MaksInk', specialization: 'Реализм' },
+  { name: 'Мария', nickname: 'MashaTattoo', specialization: 'Минимализм' },
+  { name: 'Иван', nickname: 'IvanMaster', specialization: 'Традиционный' },
+  { name: 'Олег', nickname: 'OlegInk', specialization: 'Новая школа' },
+  { name: 'Екатерина', nickname: 'KateTribal', specialization: 'Татуировки в стиле трайбл' },
+  { name: 'Сергей', nickname: 'SergeiInk', specialization: 'Японский' },
+  { name: 'Павел', nickname: 'PaulGeometric', specialization: 'Геометрический' },
+  { name: 'Андрей', nickname: 'AndreiGrey', specialization: 'Черно-серый' },
+  { name: 'Денис', nickname: 'DenisTattoo', specialization: 'Минимализм' },
+  { name: 'Елена', nickname: 'ElenaBio', specialization: 'Биомеханика' },
+]
+
+@tattoos_data = [
+  { title: 'Иллюзия вечернего света', specialization: 'Гипноз', master_id: 1 },
+  { title: 'Абстрактный космос', specialization: 'Эксперимент', master_id: 2 },
+  { title: 'Сон в горах', specialization: 'Классика', master_id: 3 },
+  { title: 'Поцелуй Луны', specialization: 'Мистика', master_id: 4 },
+  { title: 'Механический город', specialization: 'Ретро', master_id: 5 },
+  { title: 'Племенные символы', specialization: 'Этника', master_id: 6 },
+  { title: 'Сакура в ветрах', specialization: 'Япония', master_id: 7 },
+  { title: 'Расплывчатые краски', specialization: 'Импрессионизм', master_id: 8 },
+  { title: 'Геометрия в деталях', specialization: 'Абстракция', master_id: 9 },
+  { title: 'Черная магия', specialization: 'Оккультизм', master_id: 10 },
+  { title: 'Амфибия', specialization: 'Эксперимент', master_id: 1 },
+  { title: 'Аксиома времени', specialization: 'Ретро', master_id: 2 },
+  { title: 'Вечерняя мелодия', specialization: 'Мистика', master_id: 3 },
+  { title: 'Заводной робот', specialization: 'Этника', master_id: 4 },
+  { title: 'Солнце и луна', specialization: 'Япония', master_id: 5 },
+  { title: 'Акварельные мечты', specialization: 'Импрессионизм', master_id: 6 },
+  { title: 'Графика в стиле Пикассо', specialization: 'Абстракция', master_id: 7 },
+  { title: 'Сказочный пейзаж', specialization: 'Классика', master_id: 8 },
+  { title: 'Древний рунный знак', specialization: 'Оккультизм', master_id: 9 },
+  { title: 'Электрический ангел', specialization: 'Ретро', master_id: 10 },
+  { title: 'Сон в лесу', specialization: 'Мистика', master_id: 1 },
+  { title: 'Живопись без границ', specialization: 'Абстракция', master_id: 2 },
+  { title: 'Шаманский талисман', specialization: 'Этника', master_id: 3 },
+  { title: 'Традиции Древнего Востока', specialization: 'Япония', master_id: 4 },
+  { title: 'Рисунок на берегу озера', specialization: 'Импрессионизм', master_id: 5 },
+  { title: 'Астральный лабиринт', specialization: 'Оккультизм', master_id: 6 },
+  { title: 'Текстуры и абстракции', specialization: 'Абстракция', master_id: 7 },
+  { title: 'Мастерская живописи', specialization: 'Классика', master_id: 8 },
+  { title: 'Руны и магия', specialization: 'Оккультизм', master_id: 9 },
+  { title: 'Символы ветра', specialization: 'Этника', master_id: 10 },
+  { title: 'Легендарный дракон', specialization: 'Мистика', master_id: 1 },
+  { title: 'Гиперреализм в черно-белом', specialization: 'Абстракция', master_id: 2 },
+  { title: 'Сказка на коже', specialization: 'Япония', master_id: 3 },
+  { title: 'Акварельные сны', specialization: 'Импрессионизм', master_id: 4 },
+  { title: 'Часы на руке', specialization: 'Ретро', master_id: 5 },
+  { title: 'Иллюзии мозга', specialization: 'Эксперимент', master_id: 6 },
+  { title: 'Геометрические структуры', specialization: 'Абстракция', master_id: 7 },
+  { title: 'Тайны океана', specialization: 'Этника', master_id: 8 },
+  { title: 'Тропики и живопись', specialization: 'Импрессионизм', master_id: 9 },
+  { title: 'Магический портал', specialization: 'Оккультизм', master_id: 10 }
+]
+
+def seed
+  reset_db
+  create_masters(@masters_data)
+  create_tattoos(@tattoos_data)
+end
+
+def reset_db
+  Rake::Task['db:drop'].invoke
+  Rake::Task['db:create'].invoke
+  Rake::Task['db:migrate'].invoke
+end
+
+def create_masters(data)
+  data.shuffle.each do |master_data|
+    master = Master.create(name: master_data[:name], nickname: master_data[:nickname], specialization: master_data[:specialization])
+    puts "Master with id #{master.id} just created"
+  end
+end
+
+def create_tattoos(data)
+  data.shuffle.each do |tattoo_data|
+    master_id = tattoo_data[:master_id] || Master.pluck(:id).sample
+    tattoo = Tattoo.create(title: tattoo_data[:title], specialization: tattoo_data[:specialization], master_id: master_id)
+    puts "Tattoo with id #{tattoo.id} for master with id #{tattoo.master.id} just created"
+  end
+end
+
+seed
