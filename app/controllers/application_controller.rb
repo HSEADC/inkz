1b2337ac8fc83b5e0fa19007d252702c00726b50
bozzhik <bozzhik@ya.ru>
2023-10-19 02:35:04 +0300

    add redirect to `tattoos_path`
    add redirect after `access_denied` and `log in` + `log out`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to tattoos_path, notice: exception.message }
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    tattoos_path
+  end
+
+  def after_sign_out_path_for(resource)
+    tattoos_path
+  end
 end
