514a0dc21834c9b178f4bffe0c222cc153631d0c
bozzhik <bozzhik@ya.ru>
2023-10-22 16:45:39 +0300

    finalize `masters` for admin `namespace`

--- app/controllers/admin/masters_controller.rb
+++ app/controllers/admin/masters_controller.rb
@@ -1,43 +1,33 @@
 class Admin::MastersController < ApplicationController
   load_and_authorize_resource
-  before_action :authenticate_user!, except: [:index, :show]  # Allow unauthenticated access to index and show
+  before_action :authenticate_admin! # Make sure the user is an admin
 
-  # GET /masters or /masters.json
+  # GET /admin/masters or /admin/masters.json
   def index
-    if current_user&.is_master?
-      # User has master privileges, display all masters
-      @user_masters = Master.where(user: current_user)
-      @other_masters = Master.where.not(user: current_user)
-    else
-      # User does not have master privileges, display only their master if it exists
-      @user_masters = [current_user&.master].compact
-      @other_masters = Master.where.not(user: current_user)
-    end
-    @masters = @user_masters + @other_masters
+    @masters = Master.all
   end
 
-  # GET /masters/1 or /masters/1.json
+  # GET /admin/masters/1 or /admin/masters/1.json
   def show
     @tattoos = @master.tattoos
   end
 
-  # GET /masters/new
+  # GET /admin/masters/new
   def new
     @master = Master.new
   end
 
-  # GET /masters/1/edit
+  # GET /admin/masters/1/edit
   def edit
   end
 
-  # POST /masters or /masters.json
+  # POST /admin/masters or /admin/masters.json
   def create
     @master = Master.new(master_params)
-    # @master.user = current_user # Set the user for this master
 
     respond_to do |format|
       if @master.save
-        format.html { redirect_to master_url(@master), notice: "Master was successfully created." }
+        format.html { redirect_to admin_master_url(@master), notice: "Master was successfully created." }
         format.json { render :show, status: :created, location: @master }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -46,28 +36,7 @@ class Admin::MastersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /masters/1 or /masters/1.json
-  def update
-    respond_to do |format|
-      if @master.update(master_params)
-        format.html { redirect_to master_url(@master), notice: "Master was successfully updated." }
-        format.json { render :show, status: :ok, location: @master }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @master.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /masters/1 or /masters/1.json
-  def destroy
-    @master.destroy
-
-    respond_to do |format|
-      format.html { redirect_to masters_url, notice: "Master was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  # ...
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -77,6 +46,10 @@ class Admin::MastersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def master_params
-      params.require(:master).permit(:name, :nickname, :specialization).merge(user_id: current_user.id)
+      params.require(:master).permit(:name, :nickname, :specialization)
+    end
+
+    def authenticate_admin!
+      redirect_to root_path, alert: "Access denied." unless current_user&.is_admin?
     end
 end
