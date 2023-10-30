ab4ee9e24c54e35b80aafb8679a395240990f27e
bozzhik <bozzhik@ya.ru>
2023-10-30 19:44:55 +0300

    upload 'SuisseIntl' & `Angst` fonts

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['SuisseIntl', ...defaultTheme.fontFamily.sans],
+        ink: ['Angst'],
       },
     },
   },
