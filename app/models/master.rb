3d82dc368024c6dc614345dcb484a7c7cdb7dbec
bozzhik <bozzhik@ya.ru>
2023-12-03 02:17:38 +0200

    add `validates` for models attributes

--- app/models/master.rb
+++ app/models/master.rb
@@ -1,4 +1,8 @@
 class Master < ApplicationRecord
   belongs_to :user
   has_many :tattoos, dependent: :destroy
+
+  validates :name, presence: true
+  validates :nickname, presence: true
+  validates :user_id, presence: true
 end
--- app/models/tattoo.rb
+++ app/models/tattoo.rb
@@ -2,6 +2,7 @@ class Tattoo < ApplicationRecord
   belongs_to :master
   belongs_to :user, optional: true
 
-
   mount_uploader :tattoo_image, TattooImageUploader
+
+  validates :title, presence: true
 end
