4db6209f2fcabc692464d5c307d931575ae7b925
bozzhik <bozzhik@ya.ru>
2023-12-04 03:29:59 +0200

    optimize using `FeedbackHelper`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def can_modify_feedback?(feedback)
-    current_user&.id == feedback.user_id || (current_user&.master&.id == feedback.master_id if current_user&.master)
-  end
 end
--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -1,2 +1,5 @@
 module FeedbacksHelper
+  def can_modify_feedback?(feedback)
+    current_user&.id == feedback.user_id || (current_user&.master&.id == feedback.master_id if current_user&.master)
+  end
 end
