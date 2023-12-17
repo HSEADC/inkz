3afca826d9de2ea6936d834706de0f1afe7f9f7f
bozzhik <bozzhik@ya.ru>
2023-12-17 17:10:42 +0300

    change dev server `port` to `:2000`

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 2000.
 #
-port ENV.fetch("PORT") { 1000 }
+port ENV.fetch("PORT") { 2000 }
 
 # Specifies the `environment` that Puma will run in.
 #
