4eafb0f6294cb158826c68fd5e04ff1f602327ac
bozzhik <bozzhik@ya.ru>
2023-12-19 05:23:33 +0300

    optimize `seeds.rb`

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 def seed
   reset_db
-  create_users(14)
-  create_admin
+  create('Admin') { create_admin }
+  create('Users + Masters') { create_users_and_masters(14) }
+  create('Tattoos') { create_tattoos }
+  create('Feedbacks') { create_feedbacks }
+  create('Subscriptions') { create_subscriptions }
+end
 
-  create_masters(10)
-  create_feedbacks
-  create_tattoos
-  create_subscriptions
+def create(model)
+  puts("-----[#{model}]-----")
+  yield
 end
 
 def reset_db
@@ -15,8 +18,14 @@ def reset_db
   Rake::Task['db:migrate'].invoke
 end
 
-def create_users(num_users)
-  (1...num_users).map do |i|
+def create_admin
+  user = User.create!(email: "admin@inkz.ru", password: 'inkzzz', is_admin: true)
+  puts "Admin with #{user.email} created with id #{user.id}"
+end
+
+def create_users_and_masters(num_users)
+  users = []
+  (1..num_users).each do |i|
     is_master = (i <= 10)
     user_data = {
       email: "user#{i}@inkz.ru",
@@ -25,36 +34,55 @@ def create_users(num_users)
     }
 
     user = User.create!(user_data)
-    puts "User with #{user.email} created with id #{user.id}. Is Master: #{is_master ? 'Yes' : 'No'}"
+    users << user
+
+    if is_master
+      master_data = {
+        name: Faker::Name.name,
+        nickname: Faker::Internet.username(specifier: 5..10),
+        specialization: Faker::Games::WorldOfWarcraft.class_name,
+        user_id: user.id
+      }
+
+      master = Master.create!(master_data)
+      puts "User [#{user.email}] and Master [#{master.id}] just created. Is Master: Yes"
+    else
+      puts "User [#{user.email}] just created. Is Master: No"
+    end
   end
+
+  users
 end
 
-def create_admin
-  user = User.create!(email: "admin@inkz.ru", password: 'inkzzz', is_admin: true)
-  puts "Admin with #{user.email} created with id #{user.id}"
-end
-
-def create_masters(num_masters)
-  users = User.where(is_master: true).to_a
-  users = users.cycle
-
-  num_masters.times do
-    user = users.next
-    master = Master.create(
-      name: Faker::Name.name,
-      nickname: Faker::Internet.username(specifier: 5..10),
-      specialization: Faker::Games::WorldOfWarcraft.class_name,
-      user_id: user.id
-    )
-
-    puts "Master with id #{master.id} just created for user #{user.id}"
-  end
+# ссылка на изображения tattoos // https://disk.yandex.ru/d/PTdfE03I45aN2w
+def upload_random_image
+  uploader = TattooImageUploader.new(Tattoo.new, :tattoo_image)
+  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'public/autoupload/tattoos', '*')).sample))
+  uploader
 end
 
 def upload_random_feedback_image
   uploader = FeedbackImageUploader.new(Feedback.new, :feedback_image)
   uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'public/autoupload/tattoos', '*')).sample))
   uploader
+end
+
+def create_tattoos
+  masters = Master.all
+
+  masters.each do |master|
+    2.times do
+      tattoo = Tattoo.create(
+        title: Faker::Games::WorldOfWarcraft.hero,
+        specialization: Faker::Games::WorldOfWarcraft.class_name,
+        master_id: master.id,
+        tattoo_image: upload_random_image,
+        user_id: master.user.id
+      )
+
+      puts "Tattoo [#{tattoo.id}] for Master #{tattoo.master.id} just created. Title: #{tattoo.title}, Specialization: #{tattoo.specialization}"
+    end
+  end
 end
 
 def create_feedbacks(num_feedbacks = 2)
@@ -75,32 +103,7 @@ def create_feedbacks(num_feedbacks = 2)
       end
 
       feedback = Feedback.create!(feedback_data)
-      puts "Feedback with id #{feedback.id} for Master with id #{master.id} just created with rating #{feedback.rating}"
-    end
-  end
-end
-
-# ссылка на изображения tattoos // https://disk.yandex.ru/d/PTdfE03I45aN2w
-def upload_random_image
-  uploader = TattooImageUploader.new(Tattoo.new, :tattoo_image)
-  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'public/autoupload/tattoos', '*')).sample))
-  uploader
-end
-
-def create_tattoos(num_tattoos = 2)
-  masters = Master.all
-
-  masters.each do |master|
-    num_tattoos.times do
-      tattoo = Tattoo.create(
-        title: Faker::Games::WorldOfWarcraft.hero,
-        specialization: Faker::Games::WorldOfWarcraft.class_name,
-        master_id: master.id,
-        tattoo_image: upload_random_image,
-        user_id: master.user.id
-      )
-
-      puts "Tattoo with id #{tattoo.id} for master with id #{tattoo.master.id} just created. Title: #{tattoo.title}, Specialization: #{tattoo.specialization}"
+      puts "Feedback [#{feedback.id}] for Master #{master.id} just created with rating [#{feedback.rating}]"
     end
   end
 end
@@ -109,7 +112,7 @@ def create_subscriptions(num_users = 5)
   num_users.times do
     email = Faker::Internet.email
     subscription = Subscription.create(email: email)
-    puts "Subscription with email #{subscription.email} just created"
+    puts "Subscription [#{subscription.email}] just created."
   end
 end
 
