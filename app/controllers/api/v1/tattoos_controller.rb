51d569ecb6477b4c9faf3adea9f2bc5f49335adb
bozzhik <bozzhik@ya.ru>
2024-03-17 11:08:16 +0300

    temporary `API` configuration for `auth`

--- app/controllers/api/v1/tattoos_controller.rb
+++ app/controllers/api/v1/tattoos_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::TattoosController < Api::V1::ApplicationController
 
   def index
     @tattoos = Tattoo.all
-    @new_tattoo_url = new_api_v1_master_tattoo_url(master_id: 1)
+    # @new_tattoo_url = new_api_v1_master_tattoo_url(master_id: 1)
   end
 
   def show
@@ -15,6 +15,32 @@ class Api::V1::TattoosController < Api::V1::ApplicationController
 
     render json: { parts: @parts_tags, styles: @styles_tags }
   end
+
+  def create
+    jti = request.headers['Authorization']
+    @user = User.find_by_jti(jti)
+
+    if @user.present?
+      @master = @user.master
+
+      @tattoo = @master.tattoos.new(title: params[:tattoo][:title], master_id: params[:tattoo][:master_id])
+      @tattoo.user = current_user
+
+      if params[:tattoo][:tattoo_image].present?
+        @tattoo.tattoo_image.attach(params[:tattoo][:tattoo_image])
+      end
+
+      if @tattoo.save
+        render json: @tattoo.as_json, status: :created
+      else
+        puts @tattoo.errors.full_messages # Add this line for debugging
+        render json: @tattoo.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
+  end
+
 
   # def by_part
   #   @tags = Tattoo.tag_counts_on(:parts).pluck(:name)
@@ -37,4 +63,14 @@ class Api::V1::TattoosController < Api::V1::ApplicationController
       @tattoos = Tattoo.all
     end
   end
+
+  # Only allow a list of trusted parameters through.
+  # def tattoo_params
+  #   params.require(:tattoo).permit(:title, :specialization, :tattoo_image, :part_list, :style_list)
+  # end
+
+  def tattoo_params
+    params.require(:tattoo).permit(:title, :part_list, :style_list, :master_id)
+  end
+
 end
--- app/views/api/v1/tattoos/index.json.jbuilder
+++ app/views/api/v1/tattoos/index.json.jbuilder
@@ -1,4 +1,5 @@
-json.set! :new_url, @new_tattoo_url
+json.set! :create_url, api_v1_tattoos_url
+json.set! :jti, User.find_by_email("user1@inkz.ru").jti
 
 json.set! :tattoos do
   json.array! @tattoos, partial: "api/v1/tattoos/tattoo", as: :tattoo
