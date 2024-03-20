f53d8f3ecde4d0a7cd4ed00a88575e0f8d34fdf0
bozzhik <bozzhik@ya.ru>
2024-03-20 19:31:48 +0300

    fix uploading image from external `form` via api

--- app/controllers/api/v1/tattoos_controller.rb
+++ app/controllers/api/v1/tattoos_controller.rb
@@ -23,26 +23,22 @@ class Api::V1::TattoosController < Api::V1::ApplicationController
     if @user.present?
       @master = @user.master
 
-      @tattoo = @master.tattoos.new(title: params[:tattoo][:title], master_id: params[:tattoo][:master_id])
+      @tattoo = @master.tattoos.new(tattoo_params)
       @tattoo.user_id = @user.id
 
       if params[:tattoo][:tattoo_image].present?
-        @tattoo.tattoo_image.attach(params[:tattoo][:tattoo_image])
+        @tattoo.tattoo_image = params[:tattoo][:tattoo_image]
       end
-
-      puts @tattoo.to_json
 
       if @tattoo.save
         render json: @tattoo.as_json, status: :created
       else
-        puts @tattoo.errors.full_messages # Add this line for debugging
         render json: @tattoo.errors, status: :unprocessable_entity
       end
     else
       render json: { error: 'User not found' }, status: :not_found
     end
   end
-
 
   # def by_part
   #   @tags = Tattoo.tag_counts_on(:parts).pluck(:name)
@@ -72,7 +68,11 @@ class Api::V1::TattoosController < Api::V1::ApplicationController
   # end
 
   def tattoo_params
-    params.require(:tattoo).permit(:title, :part_list, :style_list, :master_id)
+    params.require(:tattoo).permit(:title, :master_id, :tattoo_image)
   end
+
+  # def tattoo_params
+  #   params.require(:tattoo).permit(:title, :part_list, :style_list, :master_id)
+  # end
 
 end
