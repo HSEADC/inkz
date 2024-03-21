e07809577ba96c5eb694015a2459c8d29f61570b
bozzhik <bozzhik@ya.ru>
2024-03-21 03:02:01 +0300

    remake `sign_up` via api

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,13 +1,16 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
   skip_before_action :verify_authenticity_token
 
+  # sign up
   def create
     user = User.new user_params
+
     if user.save
       render json: {
         messages: "Sign Up Successfully",
         is_success: true,
-        jwt: encrypt_payload
+        data: {user: user}
+        # jwt: encrypt_payload
       }, status: :ok
     else
       render json: {
@@ -24,8 +27,8 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
-  def encrypt_payload
-    payload = @user.as_json(only: [:email, :jti])
-    token = JWT.encode payload, Rails.application.credentials.devise_jwt_secret_key!, 'HS256'
-  end
+  # def encrypt_payload
+  #   payload = @user.as_json(only: [:email, :jti])
+  #   token = JWT.encode payload, Rails.application.credentials.devise_jwt_secret_key!, 'HS256'
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
       # resources :subscriptions
 
       devise_scope :user do
-        # post "sign_up", to: "registrations#create"
+        post "sign_up", to: "registrations#create"
         post "sign_in", to: "sessions#create"
         post "sign_out", to: "sessions#destroy"
       end
