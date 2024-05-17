ac43776287575dfffe907ce59997c90594795c56
bozzhik <bozzhik@ya.ru>
2024-05-17 05:19:39 +0300

    ~ fix dumb code errors

--- app/jobs/wait_and_make_job.rb
+++ app/jobs/wait_and_make_job.rb
@@ -2,7 +2,7 @@ class WaitAndMakeJob < ApplicationJob
   queue_as :default
 
   def perform(master, user)
-    master.feedbacks.create!(comment: 'Привет!', master_id: master.id user_id: user.id)
+    master.feedbacks.create!(comment: 'Привет!', master_id: master.id, user_id: user.id)
     Rails.logger.info("\e[1;33mWAKE AND MAKE JOB\e[0m")
   end
 end
