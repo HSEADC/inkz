e0c371e75ae35cd49474d5cbf4d7f4d6a754f0de
bozzhik <bozzhik@ya.ru>
2024-06-21 17:21:12 +0300

    refactor `[Container, Heading]` components (view-components)

--- app/components/global/container_component.rb
+++ app/components/global/container_component.rb
@@ -1,6 +1,8 @@
-# frozen_string_literal: true
-
 class Global::ContainerComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <main class="<%= @className %>"><%= content %></main>
+  ERB
+
   def initialize()
     @className = 'w-[90vw] mx-auto'
   end
--- app/components/ui/heading_component.rb
+++ app/components/ui/heading_component.rb
@@ -1,9 +1,10 @@
-# frozen_string_literal: true
-
 class Ui::HeadingComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <h1 class="<%= @className %>"><%= raw @text %></h1>
+  ERB
+
   def initialize(text:, className:)
     @text = text
     @className = className
   end
-
 end
