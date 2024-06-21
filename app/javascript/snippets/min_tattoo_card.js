1820a5bb4e47cc6eedf7f896b58b75dba1e3af2d
bozzhik <bozzhik@ya.ru>
2024-06-22 02:55:25 +0300

    add `js` functionality for `MinTattooCard` welcome index component

---
+++ app/javascript/snippets/min_tattoo_card.js
@@ -0,0 +1,16 @@
+import VanillaTilt from 'vanilla-tilt'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll("[data-tattoo='card']")
+  const elementsArray = Array.from(elements)
+
+  VanillaTilt.init(elementsArray, {
+    max: 30,
+    speed: 10000,
+    glare: true,
+    'max-glare': 0.3,
+    reverse: true,
+    reset: false,
+    scale: 1.05,
+  })
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js"
+pin "vanilla-tilt", to: "https://ga.jspm.io/npm:vanilla-tilt@1.8.1/lib/vanilla-tilt.js"
