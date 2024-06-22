33f173c39b91ba71006ed6b7b64262e4666a4cc3
bozzhik <bozzhik@ya.ru>
2024-06-22 03:32:59 +0300

    make `Overview` welcome index component (block)
    rails g component Views::Welcome::Index::Overview width

--- app/components/ui/button_component.rb
+++ app/components/ui/button_component.rb
@@ -6,7 +6,7 @@ class Ui::ButtonComponent < ViewComponent::Base
   ERB
 
   BUTTON_STYLES = {
-    default: 'w-fit px-5 py-2.5 text-lg text-center font-book border-2 duration-200 rounded-10',
+    default: 'block w-fit px-5 py-2.5 text-lg text-center font-book border-2 duration-200 rounded-10',
     primary: 'bg-black text-white border-transparent hover:opacity-85',
     secondary: 'bg-transparent text-black border-black hover:border-custom-dark-gray hover:text-custom-dark-gray'
   }.freeze
---
+++ test/components/views/welcome/index/overview_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Views::Welcome::Index::OverviewComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Views::Welcome::Index::OverviewComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
