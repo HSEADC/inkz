4924a2dc311d0aaeafda5f4bddc9d32b7c5f0582
bozzhik <bozzhik@ya.ru>
2024-06-22 05:58:55 +0300

    add `preview_mode` prop for `TattooCard` Tattoo component

--- app/components/views/tattoos/tattoo_card_component.rb
+++ app/components/views/tattoos/tattoo_card_component.rb
@@ -1,10 +1,20 @@
 class Views::Tattoos::TattooCardComponent < ViewComponent::Base
-  def initialize(tattoo:, show_tags: false)
+  PREVIEW_MODE_STYLES = {
+    true: "aspect-square h-[45vh]",
+    false: "h-auto"
+  }.freeze
+
+  def initialize(tattoo:, show_tags: false, preview_mode: false)
     @tattoo = tattoo
     @show_tags = show_tags
+    @preview_mode = preview_mode
   end
 
   def render?
     @tattoo.present?
+  end
+
+  def className
+    "#{PREVIEW_MODE_STYLES[@preview_mode]} relative group"
   end
 end
