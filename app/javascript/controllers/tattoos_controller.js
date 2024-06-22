6f8e675a7244198692cd85a0b0fe2ec2806f69f7
bozzhik <bozzhik@ya.ru>
2024-06-22 19:49:25 +0300

    init draft `mansory` layout for `/index` tattoos view

--- app/javascript/controllers/tattoos_controller.js
+++ app/javascript/controllers/tattoos_controller.js
@@ -1,34 +1,17 @@
-import { Controller } from '@hotwired/stimulus';
+import { Controller } from "@hotwired/stimulus"
+import "https://unpkg.com/@appnest/masonry-layout?module";
 
 export default class extends Controller {
-  static targets = ['nextButton'];
-
-  initialize() {
-    // console.log('initialize');
-  }
-
   connect() {
-    // console.log('connect', this.nextButtonTarget);
-
-    this.viewportHeight = window.innerHeight;
-
-    document.addEventListener('scroll', () => {
-      this.onScroll();
-    });
+    this.initMasonry()
   }
 
-  onScroll() {
-    // console.log(
-    //   'scroll',
-    //   this.viewportHeight,
-    //   this.nextButtonTarget.getBoundingClientRect().top
-    // );
-
-    if (
-      this.viewportHeight >= this.nextButtonTarget.getBoundingClientRect().top
-    ) {
-      console.log('trigger next page');
-      this.nextButtonTarget.click().remove();
-    }
+  initMasonry() {
+    document.querySelector("masonry-layout").layout();
+    new Masonry(grid, {
+      itemSelector: ".tattoo-card",
+      columnWidth: ".tattoo-card",
+      percentPosition: true,
+    })
   }
 }
