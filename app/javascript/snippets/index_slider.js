6305e02af853c8fb65e73a58bc221438b3de7511
bozzhik <bozzhik@ya.ru>
2024-06-23 16:30:45 +0300

    refactor `js` files from `/snippets` folder

--- app/javascript/snippets/index_slider.js
+++ app/javascript/snippets/index_slider.js
@@ -1,11 +1,15 @@
-import EmblaCarousel from 'embla-carousel';
-import Autoplay from 'embla-carousel-autoplay';
+import EmblaCarousel from 'embla-carousel'
+import Autoplay from 'embla-carousel-autoplay'
 
-const OPTIONS = {
-  loop: true,
-};
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.querySelector('.embla')) {
+    const emblaNode = document.querySelector('.embla')
+    const viewportNode = emblaNode.querySelector('.embla__viewport')
 
-const emblaNode = document.querySelector('.embla');
-const viewportNode = emblaNode.querySelector('.embla__viewport');
+    const OPTIONS = {
+      loop: true,
+    }
 
-const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [Autoplay({ delay: 1500 })]);
+    const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [Autoplay({delay: 1500})])
+  }
+})
--- app/javascript/snippets/min_tattoo_card.js
+++ app/javascript/snippets/min_tattoo_card.js
@@ -4,13 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const elements = document.querySelectorAll("[data-tattoo='card']")
   const elementsArray = Array.from(elements)
 
-  VanillaTilt.init(elementsArray, {
-    max: 30,
-    speed: 10000,
-    glare: true,
-    'max-glare': 0.3,
-    reverse: true,
-    reset: false,
-    scale: 1.05,
-  })
+  if (elementsArray.length > 0) {
+    VanillaTilt.init(elementsArray, {
+      max: 30,
+      speed: 10000,
+      glare: true,
+      'max-glare': 0.3,
+      reverse: true,
+      reset: false,
+      scale: 1.05,
+    })
+  }
 })
