5434362580489a66918600c56fe7a09d3bff2b66
bozzhik <bozzhik@ya.ru>
2024-06-23 18:49:54 +0300

    init TattooConfig Tattoo component (view-component)

--- app/components/ui/mark_component.rb
+++ app/components/ui/mark_component.rb
@@ -1,10 +1,11 @@
 class Ui::MarkComponent < ViewComponent::Base
   erb_template <<-ERB
-    <mark class="<%= @className %>"><%= raw @text %></mark>
+    <mark id="<%= @id %>" class="<%= @className %>"><%= raw @text %></mark>
   ERB
 
-  def initialize(text:, **className)
+  def initialize(text:, id: '', **className)
     @className = ['px-2 py-1', *className.values].compact.join(' ')
+    @id = id
     @text = text
   end
 end
