756a88f5e4c1cb2addb5bb72a721e1f6599de623
bozzhik <bozzhik@ya.ru>
2024-06-25 03:47:13 +0300

    add `padding-bottom` for `Container` Global component

--- app/components/global/container_component.rb
+++ app/components/global/container_component.rb
@@ -4,6 +4,6 @@ class Global::ContainerComponent < ViewComponent::Base
   ERB
 
   def initialize()
-    @className = "#{Constants::WEBSITE_WIDTH} pt-7 sm:pt-5"
+    @className = "#{Constants::WEBSITE_WIDTH} pt-7 sm:pt-5 pb-44 xl:pb-32 sm:pb-24"
   end
 end
