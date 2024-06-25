ff155de5d1d42d5846cd154ba8d53196f301965c
bozzhik <bozzhik@ya.ru>
2024-06-25 03:27:09 +0300

    add `edit + delete` into `Tattoo` Tattoo component

--- app/components/ui/button_component.rb
+++ app/components/ui/button_component.rb
@@ -1,19 +1,20 @@
 class Ui::ButtonComponent < ViewComponent::Base
-  erb_template <<-ERB
-    <%= link_to(@href, class: @className) do %>
-      <%= @text %>
-    <% end %>
-  ERB
-
   BUTTON_STYLES = {
     default: 'block w-fit px-5 py-2.5 text-lg text-center font-book border-2 duration-200 rounded-10',
     primary: 'bg-black text-white border-transparent hover:opacity-[85%]',
-    secondary: 'bg-transparent text-black border-black hover:border-custom-dark-gray hover:text-custom-dark-gray'
+    secondary: 'bg-transparent text-black border-black hover:border-custom-dark-gray hover:text-custom-dark-gray hover:opacity-[85%]'
   }.freeze
 
-  def initialize(text:, href: nil, variant: 'primary', className: '')
+  def initialize(text:, href: nil, variant: 'primary', className: '', **options)
     @className = [BUTTON_STYLES[:default], BUTTON_STYLES[variant.to_sym], className].compact.join(' ')
     @text = text
     @href = href
+    @options = options
+  end
+
+  def call
+    link_to @href, class: @className, **@options do
+      @text
+    end
   end
 end
--- app/components/views/tattoos/tattoo_component.rb
+++ app/components/views/tattoos/tattoo_component.rb
@@ -1,9 +1,10 @@
-# frozen_string_literal: true
-
 class Views::Tattoos::TattooComponent < ViewComponent::Base
+  include Devise::Controllers::Helpers
+  include CanCan::ControllerAdditions
+  include TattoosHelper
+
   def initialize(tattoo:, master:)
     @tattoo = tattoo
-    @master = master 
+    @master = master  
   end
-
 end
